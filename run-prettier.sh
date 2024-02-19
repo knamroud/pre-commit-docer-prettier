97a320f89a3e43a250a8c143494e6ca7b87a199f
Kirelos Namroud <namroudkirelos04@gmail.com>
2024-02-19 10:08:29 +0100

    Make sure there are no more containers before deleting image

--- run-prettier.sh
+++ run-prettier.sh
@@ -2,4 +2,8 @@
 echo "FROM node:18-alpine \nWORKDIR /app\nRUN npm install -g prettier" > Dockerfile
 docker build -t prettier:tmp .
 rm Dockerfile
-docker run -v $(pwd):/app -w /app prettier:tmp prettier "$@" && docker rmi prettier:tmp -f
+docker run -v $(pwd):/app -w /app prettier:tmp prettier "$@"
+while [ ! -z "$(docker ps -a -q -f ancestor=prettier:tmp)" ]; do
+    sleep 1
+done
+docker rmi prettier:tmp
